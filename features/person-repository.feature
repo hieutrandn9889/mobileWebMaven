24f858d398cd1ca562cb9252fbc6e23f1840ba0f
hieutran <hieu.tran@enouvo.com>
2019-03-09 16:59:17 +0700

    add

---
+++ features/person-repository.feature
@@ -0,0 +1,11 @@
+Feature: hieutran
+
+  Scenario: hieutest
+    Given I hello worldaa
+    When I hello worldbb
+    Then I hello worldcc
+
+  Scenario: hieutest123
+    Given I hello world1
+    When I hello world2
+    Then I hello world3
---
+++ target/site/cucumber-pretty/report.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("person-repository.feature");
+formatter.feature({
+  "line": 1,
+  "name": "hieutran",
+  "description": "",
+  "id": "hieutran",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "hieutest",
+  "description": "",
+  "id": "hieutran;hieutest",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I hello worldaa",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I hello worldbb",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I hello worldcc",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.i_am_on_ZingPoll_website()"
+});
+formatter.result({
+  "duration": 146293356,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.i_click_the_SignIn_button()"
+});
+formatter.result({
+  "duration": 66559,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.i_input_username_and_password()"
+});
+formatter.result({
+  "duration": 472905,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "hieutest123",
+  "description": "",
+  "id": "hieutran;hieutest123",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "I hello world1",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 10,
+  "name": "I hello world2",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 11,
+  "name": "I hello world3",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefinitions.i_am_on_ZingPoll_website1()"
+});
+formatter.result({
+  "duration": 148874,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.i_click_the_SignIn_button2()"
+});
+formatter.result({
+  "duration": 669769,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefinitions.i_input_username_and_password3()"
+});
+formatter.result({
+  "duration": 94537,
+  "status": "passed"
+});
+});
