b25308dd2bb6fd86fd7f7f4ea9067473095e7a7b
Hiếu Văn Trần <hieu.tran@enouvo.com>
2020-04-30 14:16:34 +0700

    update

--- target/site/cucumber-pretty/report.js
+++ target/site/cucumber-pretty/report.js
@@ -16,7 +16,7 @@ formatter.scenarioOutline({
   "tags": [
     {
       "line": 4,
-      "name": "@Firefox"
+      "name": "@Chrome"
     }
   ]
 });
@@ -61,7 +61,7 @@ formatter.examples({
   "keyword": "Examples"
 });
 formatter.before({
-  "duration": 7678197564,
+  "duration": 2096909046,
   "status": "passed"
 });
 formatter.scenario({
@@ -74,7 +74,7 @@ formatter.scenario({
   "tags": [
     {
       "line": 4,
-      "name": "@Firefox"
+      "name": "@Chrome"
     }
   ]
 });
@@ -101,14 +101,14 @@ formatter.match({
   "location": "TC_RegisterPageSteps_ZP.i_am_on_ZingPoll_website()"
 });
 formatter.result({
-  "duration": 6118481883,
+  "duration": 9319296080,
   "status": "passed"
 });
 formatter.match({
   "location": "TC_RegisterPageSteps_ZP.i_click_the_SignIn_button()"
 });
 formatter.result({
-  "duration": 5376962820,
+  "duration": 5355015081,
   "status": "passed"
 });
 formatter.match({
@@ -125,11 +125,137 @@ formatter.match({
   "location": "TC_RegisterPageSteps_ZP.i_input_username_and_password(String,String)"
 });
 formatter.result({
-  "duration": 4265157369,
+  "duration": 4426151629,
   "status": "passed"
 });
 formatter.after({
-  "duration": 3799747488,
+  "duration": 410887758,
+  "status": "passed"
+});
+formatter.scenarioOutline({
+  "line": 15,
+  "name": "Register with empty new user11",
+  "description": "",
+  "id": "register-to-zingpoll;register-with-empty-new-user11",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 14,
+      "name": "@Chrome"
+    }
+  ]
+});
+formatter.step({
+  "line": 16,
+  "name": "I am on ZingPoll website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click the SignIn button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "I input username \u003cusername\u003e and password \u003cpassword\u003e",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 20,
+  "name": "",
+  "description": "",
+  "id": "register-to-zingpoll;register-with-empty-new-user11;",
+  "rows": [
+    {
+      "cells": [
+        "username",
+        "password"
+      ],
+      "line": 21,
+      "id": "register-to-zingpoll;register-with-empty-new-user11;;1"
+    },
+    {
+      "cells": [
+        "hieutrandn01@gmail.com",
+        "123456"
+      ],
+      "line": 22,
+      "id": "register-to-zingpoll;register-with-empty-new-user11;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 1370616586,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 22,
+  "name": "Register with empty new user11",
+  "description": "",
+  "id": "register-to-zingpoll;register-with-empty-new-user11;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 14,
+      "name": "@Chrome"
+    }
+  ]
+});
+formatter.step({
+  "line": 16,
+  "name": "I am on ZingPoll website",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click the SignIn button",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 18,
+  "name": "I input username hieutrandn01@gmail.com and password 123456",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TC_RegisterPageSteps_ZP.i_am_on_ZingPoll_website()"
+});
+formatter.result({
+  "duration": 5052238534,
+  "status": "passed"
+});
+formatter.match({
+  "location": "TC_RegisterPageSteps_ZP.i_click_the_SignIn_button()"
+});
+formatter.result({
+  "duration": 5280454466,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "hieutrandn01@gmail.com",
+      "offset": 17
+    },
+    {
+      "val": "123456",
+      "offset": 53
+    }
+  ],
+  "location": "TC_RegisterPageSteps_ZP.i_input_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 4401117336,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 170152252,
   "status": "passed"
 });
 });
